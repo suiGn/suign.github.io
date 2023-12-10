4829c937021b07ee512495617ae1a1ebc5c25a11
Sui Gn <natsynapses@gmail.com>
2023-12-10 17:41:42 -0600

    -
    -

--- assets/scripts/@packages.js
+++ assets/scripts/@packages.js
@@ -12,9 +12,13 @@ function fetchAllRepos(url) {
             }
             return response.json();
         })
-        .then(data => {
-            console.log('API Response:', data); // Log the data
-            return data;
+        .then(repos => {
+            // Filter repos by repoType
+            return repos.filter(repo => repo.repoType === 'NPM Package');
+        })
+        .then(filteredRepos => {
+            // Check for the existence of package.json in each repo
+            return Promise.all(filteredRepos.map(repo => checkPackageJson(repo)));
         })
         .catch(error => {
             console.error('Error fetching repositories:', error);
@@ -22,24 +26,43 @@ function fetchAllRepos(url) {
         });
 }
 
+function checkPackageJson(repo) {
+    return fetch(`https://api.github.com/repos/${username}/${repo.name}/contents/package.json`)
+        .then(response => {
+            if (response.status === 200) {
+                return repo; // package.json exists, return repo data
+            } else {
+                return null; // package.json does not exist, filter out this repo
+            }
+        })
+        .catch(() => null); // In case of any error, filter out this repo
+}
+
 
 function createRepoHtml(repoData) {
-    // Create the main container div
     const repoDiv = document.createElement('div');
     repoDiv.className = 'pck w-clearfix';
 
-    // Construct the inner HTML using repoData
+    // Construct the package.json URL and docs URL
+    const packageJsonUrl = `https://api.github.com/repos/${username}/${repoData.name}/contents/package.json`;
+    const docsUrl = `https://${username}.github.io/${repoData.name}`;
+
+    // Determine the display for the homepage link
+    const homepageDisplay = repoData.homepage 
+        ? `<a href="${repoData.homepage}" class="link">Open</a>`
+        : 'noHomePage';
+
     repoDiv.innerHTML = `
+        <div class="text-block-3">Package.json: <a href="${packageJsonUrl}" class="link">Open</a></div>
+        <div class="text-block-3">Docs: <a href="${docsUrl}" class="link">Open</a></div>
         <div class="text-span-2 _111 lowest">O</div>
-        <div class="text-block-3 dataset">${repoData.uuid || 'uuid 00000000000000'}</div>
-        <div class="text-block-3">${repoData.connectionDate || 'connection.date'}</div>
-        <div class="text-block-3 datatext">${repoData.username || 'usrname'} - ${repoData.device || 'device'}</div>
-        <div class="text-block-3">${repoData.activeDuration || 'active for 00:00'}</div>
+        <div class="text-block-3 dataset">${repoData.id || 'id 00000000000000'}</div>
+        <div class="text-block-3 datatext">${repoData.name || 'name'} - ${homepageDisplay}</div>
+        <div class="text-block-3">${repoData.repoType || 'npm package'}</div>
         <div class="pck-viewall">[ view]</div>
+        
         <div class="vwmtd">
             <div class="scrollable-cleaker-text openconnection">
-                Package: ${repoData.packageJson || 'package.json'},<br>
-                main: ${repoData.main || '2:12:45pm 06-16-2019'},<br>
                 npm: <a href="${repoData.npmUrl || '#'}" class="link">${repoData.npmRegistryUrl || 'npmregistryurl'}</a>,<br>
                 author: ${repoData.author || ''},<br>
             </div>
@@ -60,3 +83,21 @@ function displayRepos(repos) {
         repoListContainer.appendChild(repoHtml);
     });
 }
+
+// Define any constants
+const username = 'suiGn'; // Your GitHub username
+const reposApiUrl = `https://api.github.com/users/${username}/repos`;
+
+// ... (rest of your functions: fetchAllRepos, createRepoHtml, displayRepos) ...
+
+// Event listener to start the script when the DOM content is loaded
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Fetching repositories...');
+
+    fetchAllRepos(reposApiUrl)
+        .then(repos => repos.filter(repo => repo != null)) // Filter out nulls
+        .then(displayRepos)
+        .catch(error => {
+            console.error('Error in fetching and displaying repos:', error);
+        });
+});
